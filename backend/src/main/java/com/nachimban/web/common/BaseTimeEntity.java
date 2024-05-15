ce381cc94f6def9b6c95cb9b6f12bd470ddad03a
cpot5620 <cpot5620@gmail.com>
2024-05-15 11:10:37 +0900

    feat: BaseTimeEntity 구현

---
+++ backend/src/main/java/com/nachimban/web/common/BaseTimeEntity.java
@@ -0,0 +1,37 @@
+package com.nachimban.web.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected BaseTimeEntity(
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+    ) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+}
